f7543702fc49e14e8223d113fdc58b88a364ab8e
allisonkydy <allisnelly@gmail.com>
2019-09-30 15:06:11 -0500

    built fields, constructor, methods for customer class

--- src/snackBar/Customer.java
+++ src/snackBar/Customer.java
@@ -2,5 +2,49 @@ package snackBar;
 
 public class Customer
 {
-  
+  // fields
+  private static int maxId = 0;
+  private int id;
+  private String name;
+  private double cash;
+
+  // constructor
+  public Customer(String name, double cash)
+  {
+    maxId++;
+    id = maxId;
+    this.name = name;
+    this.cash = cash;
+  }
+
+  // methods
+  public int getId()
+  {
+    return id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public double getCash()
+  {
+    return cash;
+  }
+
+  public void addCash(double numToAdd)
+  {
+    cash += numToAdd;
+  }
+
+  public void buySnack(double total)
+  {
+    cash -= total;
+  }
 }

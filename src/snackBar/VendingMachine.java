6c97a1fbf30f6fce0afc69bea38ce8a5375dd485
allisonkydy <allisnelly@gmail.com>
2019-09-30 14:59:13 -0500

    built fields, constructor, methods for vending machine class

--- src/snackBar/VendingMachine.java
+++ src/snackBar/VendingMachine.java
@@ -2,5 +2,32 @@ package snackBar;
 
 public class VendingMachine
 {
-  
+  // fields
+  private static int maxId = 0;
+  private int id;
+  private String name;
+
+  // constructor
+  public VendingMachine(String name)
+  {
+    maxId++;
+    id = maxId;
+    this.name = name;
+  }
+
+  // methods
+  public void getId()
+  {
+    return id;
+  }
+
+  public void getName()
+  {
+    return name;
+  }
+
+  public setName(String name)
+  {
+    this.name = name;
+  }
 }
